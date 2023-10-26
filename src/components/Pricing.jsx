6b06cb747fa417e60fc7962c12966c990f6a8ebf
Navaneeth <navaneeth.suresan@gmail.com>
2023-10-26 10:49:53 +0530

    modal

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import "../assets/pricing.css";
 
 function Pricing() {
+  const price = document.getElementById("price");
+  const price2 = document.getElementById("price2");
+  const price3 = document.getElementById("price3");
+  const view = document.querySelector(".formactive");
+
+  const toggle = function () {
+    view.classList.toggle("visible");
+  };
   return (
     <div>
       <div class="pricing1 py-5 bg-light">
@@ -20,12 +28,11 @@ function Pricing() {
         </div>
 
         <div class="row mt-5 row justify-content-center">
-
           <div class="col-lg-3 col-md-6">
             <div class="card text-center card-shadow on-hover border-0 mb-4">
               <div class="card-body font-14">
                 <h5 class="mt-3 mb-1 font-weight-medium">FREE</h5>
-                
+
                 <div class="pricing my-3">
                   <span class="monthly display-5 font-weight-medium">$0</span>
                   <span class="yearly display-5">240</span>
@@ -38,12 +45,11 @@ function Pricing() {
                   <li class="d-block py-2">Email support</li>
                   <li class="d-block py-2">Help center access</li>
                 </ul>
-                <div class="bottom-btn">
-                  <a
-                    class="btn btn-outline-primary me-2"
-                    href="#f1"
-                  >
-                    <span>Sign up for free</span>
+                <div class="bottom-btn" id="price" onClick={toggle}>
+                  <a class="btn btn-outline-primary me-2" href="#f1">
+                    <span >
+                      Sign up for free
+                    </span>
                   </a>
                 </div>
               </div>
@@ -54,7 +60,7 @@ function Pricing() {
             <div class="card text-center card-shadow on-hover border-0 mb-4">
               <div class="card-body font-14">
                 <h5 class="mt-3 mb-1 font-weight-medium">Pro</h5>
-                
+
                 <div class="pricing my-3">
                   <span class="monthly display-5 font-weight-medium">$15</span>
                   <span class="yearly display-5">240</span>
@@ -67,24 +73,22 @@ function Pricing() {
                   <li class="d-block py-2">Priority email support</li>
                   <li class="d-block py-2">Help center access</li>
                 </ul>
-                <div class="bottom-btn">
-                  <a
-                    class="btn btn-primary me-2"
-                    href="#f1"
-                  >
-                    <span>Get started</span>
+                <div class="bottom-btn" id="price2" onClick={toggle}>
+                  <a class="btn btn-primary me-2" href="#f1">
+                    <span >
+                      Get started
+                    </span>
                   </a>
                 </div>
               </div>
             </div>
           </div>
 
-   
           <div class="col-lg-3 col-md-6">
             <div class="card text-center card-shadow on-hover border-0 mb-4">
               <div class="card-body font-14">
                 <h5 class="mt-3 mb-1 font-weight-medium">Enterprise</h5>
-                
+
                 <div class="pricing my-3">
                   <span class="monthly display-5 font-weight-medium">$29</span>
                   <span class="yearly display-5">240</span>
@@ -97,19 +101,35 @@ function Pricing() {
                   <li class="d-block py-2">Phone and email support</li>
                   <li class="d-block py-2">Help center access</li>
                 </ul>
-                <div class="bottom-btn">
-                  <a
-                    class="btn btn-primary me-2"
-                    href="#f1"
-                  >
-                    <span>Contact us</span>
+                <div class="bottom-btn" id="price3" onClick={toggle}>
+                  <a class="btn btn-primary me-2" href="#f1">
+                    <span >
+                      Contact us
+                    </span>
                   </a>
                 </div>
               </div>
             </div>
           </div>
-
         </div>
+      </div>
+      <div className="formactive">
+        <form>
+          <div class="form-row">
+            <div class="col-7">
+              <input type="text" class="form-control" placeholder="Name" />
+            </div>
+            <div class="col">
+              <input type="email" class="form-control" placeholder="Email" />
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Orders" />
+            </div>
+            <div class="col">
+              <input type="text" class="form-control" placeholder="Comments" />
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
